d9f12248e17899e68909a3480823f5477a4d79df
L4HG <namezis@yandex.ru>
2019-05-28 14:05:58 +0300

    add correct df_name

--- FileHandler/handler.py
+++ FileHandler/handler.py
@@ -23,7 +23,17 @@ class ImageDB:
     image_df = None
     
     def __init__ (self, df_path):
-        self.image_df = pd.read_pickle(df_path, 'gzip')
+        try:
+            self.image_df = pd.read_pickle(df_path, 'gzip')
+        except Exception as e:
+            print(e)
+            self.image_df = pd.DataFrame()
+
+    def get_len(self):
+        if self.image_df is not None:
+            return len(self.image_df)
+        else:
+            return 0
         
     def return_file(self, file_id):
         file_data = {
@@ -44,165 +54,23 @@ class ImageDB:
             file_data['filename'] = str(e)
         return file_data
 
-async def get_image_pho(request):
-    req_size = None
-    b64 = False
-    req_ids = []
-    images_b64 = []
-    if request.method == 'GET':
-        try:
-            image_req = request.match_info['image_req'].split('_')
-        except:
-            return web.Response(body='Wrong request', content_type='text/html')
-    elif request.method == 'POST':
-        try:
-            data = await request.post()
-            image_req = data['image_req'].split('_')
-        except:
-            return web.Response(body='Wrong request', content_type='text/html')
-
-    images = []
-    start_step = image_req[0].split(':')
-    try:
-        id_row = int(start_step[0])
-    except Exception as e:
-        id_row = 0
-    if len(start_step) > 1:
-        try:
-            len_id = int(start_step[1])
-            if len_id > 20:
-                len_id = 20
-        except Exception as e:
-            len_id = 1
-            print(e)
-        req_ids = [id_row+i for i in range(len_id)]
-    else:
-        try:
-            req_ids = [int(x) for x in start_step[0].split(',')]
-        except Exception as e:
-            print(e)
-
-    if len(image_req) > 1:
-        try:
-            req_size = int(image_req[1])
-        except Exception as e:
-            pass
-            # print(e)
-
-        if len(image_req) > 2:
-            try:
-                b64 = int(image_req[2])
-            except Exception as e:
-                print(e)
-    
-    
-    if 'pho_df' not in request.app['image_dfs']:
-        request.app['image_dfs']['pho_df'] = ImageDB('all_images_pho.picklegz')
-    
-    imgByteData = None
-    for image_id in req_ids:
-        test_db_file = request.app['image_dfs']['pho_df'].return_file(image_id)
-        print(test_db_file)
-        filename = test_db_file.get('filename', None)
-        image_format = test_db_file.get('image_format', None)
-        compress_file = test_db_file.get('compress_file', None)
-        compress_format = test_db_file.get('compress_format', None)
-
-        imgByteData = None
-
-        if filename is not None:
-            if compress_format is not None:
-                archive_filename = compress_file
-                image_in_archive = filename
-                async with aiofiles.open(archive_filename, mode='rb') as f:
-                    archiveByteData = await f.read()
-                if compress_format.lower() == 'rar':
-                    zf = rarfile.RarFile(io.BytesIO(archiveByteData))
-                else:
-                    zf = zipfile.ZipFile(io.BytesIO(archiveByteData))
-                try:
-                    imgByteData = zf.read(image_in_archive)
-                    if imghdr.what(filename, h=imgByteData) is None:
-                        filename = filename + ':no image.html'
-                except KeyError:
-                    print('ERROR: Did not find {image_in_archive} in archive file')
-            else:
-                if isfile(filename):
-                    async with aiofiles.open(filename, mode='rb') as f:
-                        imgByteData = await f.read()
-
-            
-        if len(req_ids) > 1:
-            if req_size is None:
-                req_size = 1080
-
-        if req_size is not None and imgByteData is not None:
-            if imghdr.what(filename, h=imgByteData) is not None:
-                image = Image.open(io.BytesIO(imgByteData))
-                image = image_resize(image, req_size)
-                imgByteArr = io.BytesIO()
-                if len(req_ids) > 1:
-                    images.append(image)
-                image.save(imgByteArr, format='JPEG')
-                imgByteData = imgByteArr.getvalue()
-
-        if b64:
-            if imgByteData is not None:
-                new_image64 = {
-                    'image':str(base64.b64encode(imgByteData))[2:-1], 
-                    'image_df_id':image_id, 
-                    'filename': filename,
-                    'archive': compress_file
-                    }
-            else:
-                new_image64 = {
-                    'image':'', 
-                    'image_df_id':image_id, 
-                    'filename': filename,
-                    'archive': compress_file
-                    }
-            images_b64.append(new_image64)
-    
-    if len(images) > 1 and not b64:
-        widths, heights = zip(*(i.size for i in images))
-        max_width = max(widths)
-        total_height = sum(heights)
-        new_im = Image.new('RGB', (max_width, total_height))
-
-        y_offset = 0
-        for im in images:
-            new_im.paste(im, (0, y_offset))
-            y_offset += im.size[1]
-        imgByteArr = io.BytesIO()
-        new_im.save(imgByteArr, format='JPEG')
-        imgByteData = imgByteArr.getvalue()
-
-    if imgByteData is not None or len(images_b64) > 0:
-        if b64:
-            return web.json_response(data=images_b64)
-        else:
-            if len(req_ids) == 1:
-                content_type = mimetypes.guess_type(filename)[0]
-            else:
-                content_type = 'image/jpeg'
-            return web.Response(body=imgByteData, content_type=content_type)
-    else:
-        return web.Response(body='No image file', content_type='text/html')
-
 async def get_image(request):
     req_size = None
     b64 = False
     req_ids = []
     images_b64 = []
+    df_name = ''
     if request.method == 'GET':
         try:
             image_req = request.match_info['image_req'].split('_')
+            df_name = request.match_info['df_name']
         except:
             return web.Response(body='Wrong request', content_type='text/html')
     elif request.method == 'POST':
         try:
             data = await request.post()
             image_req = data['image_req'].split('_')
+            df_name = data['df_name']
         except:
             return web.Response(body='Wrong request', content_type='text/html')
 
@@ -241,157 +109,12 @@ async def get_image(request):
                 print(e)
     
     
-    if 'store_df' not in request.app['image_dfs']:
-        request.app['image_dfs']['store_df'] = ImageDB('./all_images_store.picklegz')
+    if df_name not in request.app['image_dfs']:
+        request.app['image_dfs'][df_name] = ImageDB('./all_images_{}.picklegz'.format(df_name))
     
     imgByteData = None
     for image_id in req_ids:
-        test_db_file = request.app['image_dfs']['store_df'].return_file(image_id)
-        print(test_db_file)
-        filename = test_db_file.get('filename', None)
-        image_format = test_db_file.get('image_format', None)
-        compress_file = test_db_file.get('compress_file', None)
-        compress_format = test_db_file.get('compress_format', None)
-
-        imgByteData = None
-
-        if filename is not None:
-            if compress_format is not None:
-                archive_filename = compress_file
-                image_in_archive = filename
-                async with aiofiles.open(archive_filename, mode='rb') as f:
-                    archiveByteData = await f.read()
-                if compress_format.lower() == 'rar':
-                    zf = rarfile.RarFile(io.BytesIO(archiveByteData))
-                else:
-                    zf = zipfile.ZipFile(io.BytesIO(archiveByteData))
-                try:
-                    imgByteData = zf.read(image_in_archive)
-                    if imghdr.what(filename, h=imgByteData) is None:
-                        filename = filename + ':no image.html'
-                except KeyError:
-                    print('ERROR: Did not find {image_in_archive} in archive file')
-            else:
-                if isfile(filename):
-                    async with aiofiles.open(filename, mode='rb') as f:
-                        imgByteData = await f.read()
-
-            
-        if len(req_ids) > 1:
-            if req_size is None:
-                req_size = 1080
-
-        if req_size is not None and imgByteData is not None:
-            if imghdr.what(filename, h=imgByteData) is not None:
-                image = Image.open(io.BytesIO(imgByteData))
-                image = image_resize(image, req_size)
-                imgByteArr = io.BytesIO()
-                if len(req_ids) > 1:
-                    images.append(image)
-                image.save(imgByteArr, format='JPEG')
-                imgByteData = imgByteArr.getvalue()
-
-        if b64:
-            if imgByteData is not None:
-                new_image64 = {
-                    'image':str(base64.b64encode(imgByteData))[2:-1], 
-                    'image_df_id':image_id, 
-                    'filename': filename,
-                    'archive': compress_file
-                    }
-            else:
-                new_image64 = {
-                    'image':'', 
-                    'image_df_id':image_id, 
-                    'filename': filename,
-                    'archive': compress_file
-                    }
-            images_b64.append(new_image64)
-    
-    if len(images) > 1 and not b64:
-        widths, heights = zip(*(i.size for i in images))
-        max_width = max(widths)
-        total_height = sum(heights)
-        new_im = Image.new('RGB', (max_width, total_height))
-
-        y_offset = 0
-        for im in images:
-            new_im.paste(im, (0, y_offset))
-            y_offset += im.size[1]
-        imgByteArr = io.BytesIO()
-        new_im.save(imgByteArr, format='JPEG')
-        imgByteData = imgByteArr.getvalue()
-
-    if imgByteData is not None or len(images_b64) > 0:
-        if b64:
-            return web.json_response(data=images_b64)
-        else:
-            if len(req_ids) == 1:
-                content_type = mimetypes.guess_type(filename)[0]
-            else:
-                content_type = 'image/jpeg'
-            return web.Response(body=imgByteData, content_type=content_type)
-    else:
-        return web.Response(body='No image file', content_type='text/html')
-
-async def get_image_insta(request):
-    req_size = None
-    b64 = False
-    req_ids = []
-    images_b64 = []
-    if request.method == 'GET':
-        try:
-            image_req = request.match_info['image_req'].split('_')
-        except:
-            return web.Response(body='Wrong request', content_type='text/html')
-    elif request.method == 'POST':
-        try:
-            data = await request.post()
-            image_req = data['image_req'].split('_')
-        except:
-            return web.Response(body='Wrong request', content_type='text/html')
-
-    images = []
-    start_step = image_req[0].split(':')
-    try:
-        id_row = int(start_step[0])
-    except Exception as e:
-        id_row = 0
-    if len(start_step) > 1:
-        try:
-            len_id = int(start_step[1])
-            if len_id > 20:
-                len_id = 20
-        except Exception as e:
-            len_id = 1
-            print(e)
-        req_ids = [id_row+i for i in range(len_id)]
-    else:
-        try:
-            req_ids = [int(x) for x in start_step[0].split(',')]
-        except Exception as e:
-            print(e)
-
-    if len(image_req) > 1:
-        try:
-            req_size = int(image_req[1])
-        except Exception as e:
-            pass
-            # print(e)
-
-        if len(image_req) > 2:
-            try:
-                b64 = int(image_req[2])
-            except Exception as e:
-                print(e)
-    
-    
-    if 'insta_df' not in request.app['image_dfs']:
-        request.app['image_dfs']['insta_df'] = ImageDB('./all_images_insta.picklegz')
-    
-    imgByteData = None
-    for image_id in req_ids:
-        test_db_file = request.app['image_dfs']['insta_df'].return_file(image_id)
+        test_db_file = request.app['image_dfs'][df_name].return_file(image_id)
         print(test_db_file)
         filename = test_db_file.get('filename', None)
         image_format = test_db_file.get('image_format', None)
@@ -485,26 +208,18 @@ async def get_df_len(request):
     if request.method == 'GET':
         try:
             df_name = request.match_info['df_name']
-            df_name = '././all_images_{}.picklegz'.format(df_name)
+            if df_name not in request.app['image_dfs']:
+                request.app['image_dfs'][df_name] = ImageDB('./all_images_{}.picklegz'.format(df_name))
+            
+            df_len = request.app['image_dfs'][df_name].get_len()
         except:
             return web.Response(body='Wrong request', content_type='text/html')
-    
-    try:
-        test_df = pd.read_pickle(df_name, compression='gzip')
-        df_len = len(test_df)
-    except Exception as e:
-        print(e)
-        df_len = -1
     
     return web.json_response(data={'df_len': df_len})
 
 app = web.Application()
-app.add_routes([web.get('/imageml/id/{image_req}', get_image)])
-app.add_routes([web.get('/imageml/pho_id/{image_req}', get_image_pho)])
-app.add_routes([web.get('/imageml/insta_id/{image_req}', get_image_insta)])
+app.add_routes([web.get('/imageml/id/{df_name}/{image_req}', get_image)])
 app.add_routes([web.post('/imageml/id', get_image)])
-app.add_routes([web.post('/imageml/pho_id', get_image_pho)])
-app.add_routes([web.post('/imageml/insta_id', get_image_insta)])
 app.add_routes([web.get('/imageml/df_len/{df_name}', get_df_len)])
 app['image_dfs'] = {}
 
